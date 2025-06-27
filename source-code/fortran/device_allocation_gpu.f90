e1ee946d2906cc98e046d8cfeaea7d28db217125
Geert Jan Bex <geertjan.bex@uhasselt.be>
2025-06-27 07:19:29 +0200

    Fix command line argument handling

--- source-code/fortran/device_allocation_gpu.f90
+++ source-code/fortran/device_allocation_gpu.f90
@@ -1,22 +1,31 @@
 program device_allocation_gpu
 
 use, intrinsic :: omp_lib, only : omp_get_default_device, omp_target_alloc
-use, intrinsic :: iso_c_binding, only : c_double, c_float, c_ptr, c_int, c_size_t, c_sizeof, c_f_pointer
-use, intrinsic :: iso_fortran_env, only : REAL64, REAL32
+use, intrinsic :: iso_c_binding, only : c_double, c_float, c_ptr, c_int, c_size_t, c_sizeof, c_f_pointer, c_associated
+use, intrinsic :: iso_fortran_env, only : REAL64, REAL32, error_unit
 
 implicit none
 
 integer, parameter :: rk = REAL64
 integer, parameter :: c_rk = c_double
-integer, parameter :: n = 1000
-integer, parameter :: nr_iters = 10
+integer :: n, nr_iters
 type(c_ptr) :: b_dev_ptr
 real(kind=c_rk), dimension(:), pointer :: b
 integer(kind=c_size_t) :: dbl_bytes
 integer(kind=c_int) :: dev
-real(kind=rk), dimension(n, n) :: a
+real(kind=rk), dimension(:, :), allocatable :: a
 real(kind=rk) :: total
-integer :: i, j, iter
+integer :: i, j, iter, status
+
+! Get command line arguments
+call cl_arguments(n, nr_iters)
+
+! Allocate array a
+allocate(a(n, n), stat=status)
+if (status /= 0) then
+    write (error_unit, '(A, I0, A, I0, A)') 'can not allocate ', n, ' by ', n, ' matrix'
+    stop 2
+end if
 
 ! Get device
 dev = omp_get_default_device()
@@ -36,6 +45,10 @@ end do
 ! Allocate b on device
 dbl_bytes = c_sizeof(real(1.0_rk, kind=c_rk))
 b_dev_ptr = omp_target_alloc(n*n*dbl_bytes, dev)
+if (.not. c_associated(b_dev_ptr)) then
+    write (error_unit, '(A, I0, A, I0, A)') 'can not allocate ', n, '*', n, ' device array'
+    stop 2
+end if
 
 call c_f_pointer(b_dev_ptr, b, [n*n])
 
@@ -53,7 +66,7 @@ do iter = 1, nr_iters
   !$OMP target teams distribute parallel do is_device_ptr(b) default(none) private(j) shared(a, b, n)
   do i = 1, n
     do j = 1,n
-      a(i, j)  = b((i-1)*n + j)
+      a(i, j) = b((i-1)*n + j)
     end do
   end do
   !$OMP end target teams distribute parallel do
@@ -68,6 +81,9 @@ call omp_target_free(b_dev_ptr, dev)
 ! Show resullt
 print '(A10 E25.15)', 'sum = ', sum(a)
 
+! Deallocate array a
+deallocate(a)
+
 ! Compute and show expected result
 total = 0.0_rk
 do i = 1, n
@@ -76,5 +92,38 @@ do i = 1, n
   end do
 end do
 print '(A10 E25.15)', 'expected = ', total
+
+contains
+
+    integer function get_value(i)
+        implicit none
+        integer, value :: i
+        character(len=1024) :: buffer
+
+        call get_command_argument(i, buffer)
+        read (buffer, *) get_value
+    end function get_value
+
+    subroutine cl_arguments(n, p)
+        use, intrinsic :: iso_fortran_env, only : error_unit
+        implicit none
+        integer, intent(out) :: n, p
+        integer, parameter :: n_default = 1000, p_default = 10
+
+        select case(command_argument_count())
+            case(0)
+                n = n_default
+                p = p_default
+            case(1)
+                n = get_value(1)
+                p = p_default
+            case(2)
+                n = get_value(1)
+                p = get_value(2)
+            case default
+                write (error_unit, '(A)') 'call as device_allocation_gpu [ N [ nr_iterations ] ]'
+                stop 1
+        end select
+    end subroutine cl_arguments
 
 end program device_allocation_gpu
