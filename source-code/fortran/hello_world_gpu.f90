87c799d26c2e96ff4cc67f1cce2b6a5728b83974
Geert Jan Bex <geertjan.bex@uhasselt.be>
2025-08-26 08:39:12 +0200

    Add hello world example

--- source-code/fortran/hello_world_gpu.f90
+++ source-code/fortran/hello_world_gpu.f90
@@ -1,26 +1,27 @@
 program hello_world
+    use :: omp_lib, only : omp_get_num_devices, omp_is_initial_device, omp_get_num_teams, &
+                           omp_get_num_threads
+    implicit none
+    integer :: num_devices
+    integer :: nteams
+    integer :: nthreads
 
-use OMP_lib
+    ! Get number of devices
+    num_devices = omp_get_num_devices()
+    print '(A, I0)', 'Number of available devices: ', num_devices
 
-implicit none
-
-integer :: num_devices
-integer :: nteams
-integer :: nthreads
-
-!> Get number of devices
-num_devices = omp_get_num_devices()
-write(*,*) "Number of available devices", num_devices
-
-!> Check wether additional device is found
-!$OMP target
-if (omp_is_initial_device()) then
-    write(*,*) "Running on host"
-else
-    nteams = omp_get_num_teams()
-    nthreads = omp_get_num_threads()
-    write(*,*) "Running on device with ", nteams, " teams in total and ", nthreads, " threads in each team"
-end if
-!$OMP end target
+    ! Check wether additional device is found
+    ! Note: formatted I/O is not supported on device
+    !$OMP target
+    if (omp_is_initial_device()) then
+        print *, 'Running on host'
+    else
+        nteams = omp_get_num_teams()
+        nthreads = omp_get_num_threads()
+        print *, &
+            'Running on device with ', nteams, ' teams in total and ', nthreads, &
+            ' threads in each team'
+    end if
+    !$OMP end target
 
 end program hello_world
