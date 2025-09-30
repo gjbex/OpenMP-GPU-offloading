6ce271ceec51e3a0d93db27ace219913423bc415
Geert Jan Bex <geertjan.bex@uhasselt.be>
2025-09-30 08:24:00 +0200

    Fix format

--- source-code/fortran/structured_data_region_gpu.f90
+++ source-code/fortran/structured_data_region_gpu.f90
@@ -1,48 +1,38 @@
 program structured_data_region_gpu
+    use, intrinsic :: iso_fortran_env, only : dp => REAL64, output_unit
+    implicit none
+    integer, parameter :: n = 1000, nr_iters = 10
+    real(Kind=dp), dimension(n*n) :: a, b
+    real(kind=dp) :: sumv
+    integer :: i, j, iter
 
-implicit none
+    !$OMP target data map(tofrom: a(1:n*n))
+        !$OMP target teams distribute parallel do
+        do i = 1, n*n
+            a(i) = 0.0_dp
+        end do
+        !$OMP end target teams distribute parallel do
 
-integer, parameter :: n = 1000
-integer, parameter :: nr_iters = 10
+        do i = 1,n
+            do j = 1,n
+                b((i - 1)*n + j) = 1.0_dp*((i - 1)*n + (j - 1))/n**2
+            end do
+        end do
 
-double precision :: a(n*n), b(n*n)
-double precision :: sumv
-integer :: ii, jj, iter
+        !$OMP target data map(to: b(1:n*n))
+            do iter = 1, nr_iters
+                !$OMP target teams distribute parallel do
+                do i = 1, n
+                    do j = 1,n
+                        a((i - 1)*n + j) = a((i - 1)*n + j) + b((i - 1)*n + j)
+                    end do
+                end do
+                !$OMP end target teams distribute parallel do
+            end do
+        !$OMP end target data
+    !$OMP end target data
 
-!$OMP target data map(tofrom: a(1:n*n))
-
-!$OMP target teams distribute parallel do
-do ii = 1, n
-  a((ii-1)*n + ii) = 0.d0
-end do
-!$OMP end target teams distribute parallel do
-
-do ii = 1,n
-  do jj = 1,n
-    b((ii-1)*n + jj) = 1.d0*((ii-1)*n + (jj-1))/(n*n)
-  end do
-end do
-
-
-!$OMP target data map(to: b(1:n*n))
-do iter = 1, nr_iters
-  !$OMP target teams distribute parallel do
-  do ii = 1, n
-    do jj = 1,n
-      a((ii-1)*n + jj) = a((ii-1)*n + jj) + b((ii-1)*n + jj)
-    end do
-  end do
-  !$OMP end target teams distribute parallel do
-end do
-!$OMP end target data
-!$OMP end target data
-
-sumv = 0.d0
-do ii = 1, n
-  do jj = 1, n
-    sumv = sumv + a((ii-1)*n + jj)
-  end do
-end do
-write(*,*) "sum = ", sumv
+    sumv = sum(a)
+    write(output_unit, fmt='(A, I0)') "sum = ", sumv
 
 end program structured_data_region_gpu
