7eac006ed611dc634968c6c5f6ae21e79e915b19
Geert Jan Bex <geertjan.bex@uhasselt.be>
2025-09-30 14:49:57 +0200

    Fix format

--- source-code/fortran/heat_gpu.f90
+++ source-code/fortran/heat_gpu.f90
@@ -1,94 +1,72 @@
 program heat_gpu
+    use, intrinsic :: iso_fortran_env, only : dp => REAL64
+    use, intrinsic :: omp_lib 
+    implicit none
+    integer, parameter :: n = 1000, nr_iters = 100
+    real(Kind=dp), dimension(n*n) :: temp, temp_new
+    real(kind=dp) :: temp_sum
+    integer :: iter, i, j
 
-  use OMP_lib 
+    !> Move data to device
+    !$OMP target enter data map(to:temp(1:n*n)) map(to:temp_new(1:n*n))
 
-  implicit none
-
-  integer, parameter :: n = 1000
-  integer, parameter :: nr_iters = 100
-
-  double precision :: temp(n*n), temp_new(n*n)
-  double precision :: temp_sum
-
-  integer :: it, ii, jj
-
-  !> Move data to device
-  !$OMP target enter data map(to:temp(1:n*n)) map(to:temp_new(1:n*n))
-
-  !> Initialise temperature as 0 everywhere
-  !$OMP target teams distribute parallel do
-  do ii = 1, n
-    do jj = 1, n
-      temp((ii-1)*n + jj) = 0.d0
-    end do
-  end do
-  !$OMP end target teams distribute parallel do
-
-  !> Perturb boundaries in the jj-direction, set to 1
-  !$OMP target teams distribute parallel do
-  do ii = 1, n
-    temp((ii-1)*n + 1) = 1.d0
-    temp_new((ii-1)*n + 1) = 1.d0
-
-    temp((ii-1)*n + n) = 1.d0
-    temp_new((ii-1)*n + 1) = 1.d0
-  end do
-  !$OMP end target teams distribute parallel do
-
-  !> Perturb boundaries in the ii direction, set to 1
-  !$OMP target teams distribute parallel do
-  do jj = 1, n
-    temp(0 + jj) = 1.d0
-    temp_new(0 + jj) = 1.d0
-
-    temp((n-1) + jj) = 1.d0
-    temp_new((n-1) + jj) = 1.d0
-  end do
-  !$OMP end target teams distribute parallel do
-
-  !> Start time-iteration loop
-  do it = 1, nr_iters
-  
-    !> Calculate new grid using stencil average scheme
+    !> Initialise temperature as 0 everywhere
     !$OMP target teams distribute parallel do
-    do ii = 2, n-1
-      do jj = 2, n-1
-        temp_new((ii-1)*n + jj) = 1.d0/4*(temp_new((ii-1)*n + jj - 1) &
-                                        + temp_new((ii-1)*n + jj + 1) & 
-                                        + temp_new((ii-1 - 1)*n + jj) & 
-                                        + temp_new((ii-1 + 1)*n + jj))
-      end do
+    do i = 1, n*n
+        temp(i) = 0.0_dp
     end do
     !$OMP end target teams distribute parallel do
 
-    !> Update old grid
+    !> Perturb boundaries in the j-direction, set to 1
     !$OMP target teams distribute parallel do
-    do ii = 2, n-1
-      do jj = 2, n-1
-        temp((ii-1)*n + jj) = temp_new((ii-1)*n + jj) 
-      end do
+    do i = 1, n
+        temp((i - 1)*n + 1) = 1.0_dp
+        temp_new((i - 1)*n + 1) = 1.0_dp
+        temp((i - 1)*n + n) = 1.0_dp
+        temp_new((i - 1)*n + 1) = 1.0_dp
     end do
     !$OMP end target teams distribute parallel do
 
-    !> Write output every ... iterations
-    if (modulo(it,10) .eq. 0) then
+    !> Perturb boundaries in the i direction, set to 1
+    !$OMP target teams distribute parallel do
+    do j = 1, n
+        temp(0 + j) = 1.0_dp
+        temp_new(0 + j) = 1.0_dp
+        temp(n - 1 + j) = 1.0_dp
+        temp_new(n - 1 + j) = 1.0_dp
+    end do
+    !$OMP end target teams distribute parallel do
 
-      !> Get data from device to host
-      !$OMP target update from(temp(1:n*n))
-      
-      temp_sum = 0.d0
-
-      do ii = 1, n
-        do jj = 1, n
-          temp_sum = temp_sum + temp((ii-1)*n + jj)
+    !> Start time-iteration loop
+    do iter = 1, nr_iters
+        !> Calculate new grid using stencil average scheme
+        !$OMP target teams distribute parallel do
+        do i = 2, n - 1
+            do j = 2, n - 1
+                temp_new((i - 1)*n + j) = 0.25_dp*(temp_new((i - 1)*n + j - 1) &
+                                                    + temp_new((i - 1)*n + j + 1) & 
+                                                    + temp_new((i - 1 - 1)*n + j) & 
+                                                    + temp_new((i- 1 + 1)*n + j))
+            end do
         end do
-      end do
+        !$OMP end target teams distribute parallel do
 
-      write(*,*) "iter = ", it, ", sum = ", temp_sum
+        !> Update old grid
+        !$OMP target teams distribute parallel do
+        do i = 2, n  -1
+            do j = 2, n - 1
+                temp((i - 1)*n + j) = temp_new((i - 1)*n + j) 
+            end do
+        end do
+        !$OMP end target teams distribute parallel do
 
-    end if
-
-  end do
+        !> Write output every ... iterations
+        if (mod(iter, 10) .eq. 0) then
+            !> Get data from device to host
+            !$OMP target update from(temp(1:n*n))
+            temp_sum = sum(temp)
+            print '(A, I0, A, F10.4)', 'iter = ', iter, ', sum = ', temp_sum
+        end if
+    end do
 
 end program heat_gpu
-
